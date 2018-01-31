b22c07b7fe349c2b827a7d5a565850dcb4659187
Milo Krawiec <2162979K@student.gla.ac.uk>
2018-01-31 22:36:20 +0000

    Finished chapter 9 exercises still to do

--- rango/urls.py
+++ rango/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^login/$', views.user_login, name='login'),
     url(r'^restricted/', views.restricted, name='restricted'),
+    url(r'^logout/$', views.user_logout, name='logout'),
     
     url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',
         views.add_page,
