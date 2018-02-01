689b6353ff05545dbbb2496f937840a2f73cccc1
Milo Krawiec <2162979K@student.gla.ac.uk>
2018-02-01 16:09:32 +0000

    Set up client side cookies

--- rango/views.py
+++ rango/views.py
@@ -16,6 +16,8 @@ from rango.models import Category
 
 from rango.models import Page
 
+from datetime import datetime
+
 
 def add_category(request):
     form = CategoryForm()
@@ -72,9 +74,16 @@ def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     pages_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': pages_list}
-    return render(request, 'rango/index.html', context_dict)
+
+    response = render(request, 'rango/index.html', context_dict)
+    visitor_cookie_handler(request, response)
+    return response
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+        
     context_dict = {'boldmessage': "This tutorial has been put together by Milo Krawiec"}
     return render(request, 'rango/about.html',context=context_dict)
 
@@ -140,3 +149,20 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits+1
+        response.set_cookie('last_visit',str(datetime.now()))
+    else:
+        visits = 1
+        response.set_cookie('last_visit', last_visit_cookie)
+
+    response.set_cookie('visits', visits)
+                 
